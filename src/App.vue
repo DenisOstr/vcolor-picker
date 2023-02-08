c7478d311d2bc8a162a3912bfaed9653721b10bc
Denis Ostrovsky <denisostrovskis1@gmail.com>
2023-02-08 11:48:21 +0200

    (Updated): demo

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,12 @@
             :eyeDropperEnabled="true" :onChange="color => onChange(color, 'change')" :onEndChange="color => onChange(color, 'end')" @eyeDropperEvent="eyeDropperState = $event" />
 
         <!-- <canvas id="canv" width="600" height="400" class="absolute right-4 top-4 bg-gray-200 rounded-md" ref="canvas"></canvas> -->
-        <div id="artboard" class="absolute right-5 top-5">
-            <div class="relative bg-gray-200 w-[600px] h-[400px] rounded-md">
-                <div class="absolute top-10 left-10 bg-green-400 w-32 h-32"></div>
-                <div class="absolute bottom-10 right-10 bg-violet-600 w-32 h-32 rounded-full"></div>
+        <div class="absolute right-5 top-5">
+            <div id="artboard">
+                <div class=" bg-gray-200 w-[600px] h-[400px] rounded-md">
+                    <p>Heading text</p>
+                    <svg viewBox="0 0 100 100" style="width: 100px; height: 100px; z-index: 100; opacity: 1; transform: translate(50px, 50px); fill: rgb(0, 0, 0); stroke: none; stroke-width: 0;"><path d="M0,0 h100 a0,0 0 0 1 0,0 v100 a0,0 0 0 1 -0,0 h-100 a0,0 0 0 1 -0,-0 v-100 a0,0 0 0 1 0,-0 z"></path></svg>
+                </div>
             </div>
         </div>
     </div>
@@ -29,27 +31,7 @@
 
     // Constants
     const color = ref({ red: 255, green: 0, blue: 0, alpha: 1 })
-    const canvas = ref({})
     const eyeDropperState = ref(false)
-    
-    // Mounteds
-    // onMounted(() => {
-    //     if (canvas.value) {
-    //         const ctx = canvas.value.getContext('2d')
-
-    //         ctx.beginPath()
-    //         ctx.fillStyle = '#ff0000'
-    //         ctx.rect(30, 30, 100, 100)
-
-    //         ctx.fill()
-
-    //         ctx.beginPath()
-    //         ctx.fillStyle = '#b668ff'
-    //         ctx.arc(300, 200, 100, 0, Math.PI * 2, false)
-
-    //         ctx.fill()
-    //     }
-    // })
 
     // Methods
     const onChange = (attrs, name) => {
